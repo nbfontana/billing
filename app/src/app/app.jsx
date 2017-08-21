30f821431e3b71d9e91d96e00cb884a803a838c4
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-21 17:21:01 -0300

    footer

--- app/src/app/app.jsx
+++ app/src/app/app.jsx
@@ -3,10 +3,15 @@ import '../common/template/dependencies';
 
 import Header from '../common/template/header';
 import Sidebar from '../common/template/sidebar';
+import Footer from '../common/template/footer';
 
 export default props => (
     <div className="wrapper">
         <Header/>
         <Sidebar/>
+        <div className="content-wrapper">
+            <h1>Conteudo</h1>
+        </div>
+        <Footer/>
     </div>
 )
---
+++ app/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright $copy; 2017
+            <a href="https://google.com" target="_blank">Site</a>
+        </strong>
+    </footer>
+)
