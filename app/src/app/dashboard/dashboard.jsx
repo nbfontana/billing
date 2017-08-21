1cc34bf83bcda6c238540687afae03a001d97328
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-21 18:24:31 -0300

    row

--- app/src/app/dashboard/dashboard.jsx
+++ app/src/app/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import ContentHeader from '../common/template/content-header';
 import Content from '../common/template/content';
+import ValueBox from '../common/widget/value-box';
+import Row from '../common/layout/row';
 
 class Dashboard extends Component {
     render() {
@@ -9,7 +11,11 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title="Dashboard" small="Versão 1.0"/>
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols="12 4" color="green" icon="bank" value="R$ 10" text="Total de Créditos"/>
+                        <ValueBox cols="12 4" color="red" icon="credit-card" value="R$ 10" text="Total de Débitos"/>
+                        <ValueBox cols="12 4" color="blue" icon="money" value="R$ 0" text="Valor Consolidado"/>
+                    </Row>
                 </Content>
             </div>
         );
