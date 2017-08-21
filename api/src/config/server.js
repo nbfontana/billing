b1d2eb87427e01b450f7802ac15ef260f1991288
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-21 16:04:02 -0300

    cors

--- api/src/config/server.js
+++ api/src/config/server.js
@@ -3,9 +3,11 @@ const port = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
+const allowCors = require('./cors');
 
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(port, function () {
     console.log(`API is running on port ${port}`);
