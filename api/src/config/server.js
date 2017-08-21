17f0feabba894c64d41cde48d99dad03c5577246
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-21 16:07:24 -0300

    queryParser

--- api/src/config/server.js
+++ api/src/config/server.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
 const allowCors = require('./cors');
+const queryParser = require('express-query-int');
 
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 server.use(allowCors);
+server.use(queryParser());
 
 server.listen(port, function () {
     console.log(`API is running on port ${port}`);
