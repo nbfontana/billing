74e39cd21027e471bd71ba27f9c13ee4fc0e9c90
Neri Bez Fontana <nerifontana@gmail.com>
2017-09-28 14:02:49 -0300

    Auth constants and actions

---
+++ app/src/app/auth/auth.actions.js
@@ -0,0 +1,35 @@
+import {toastr} from 'react-redux-toastr';
+import axios from 'axios';
+import constants from '../constants';
+
+export function login(values) {
+    return submit(values, `${constants.AUTH_URL}/login`);
+}
+
+export function signup(values) {
+    return submit(values, `${constants.AUTH_URL}/signup`);
+}
+
+function submit(values, url) {
+    return dispatch => {
+        axios.post(url, values).then((response) => {
+            dispatch([{type: 'USER_FETCHED', payload: response.data}]);
+        }).catch((error) => error.response.data.errors.forEach((e) => toastr.error('Erro: ', e)));
+    }
+}
+
+export function logout() {
+    return {type: 'TOKEN_VALIDATED', payload: false};
+}
+
+export function validateToken(token) {
+    return dispatch => {
+        if (token) {
+            axios.post(`${constants.AUTH_URL}/validate-token`, {token}).then((res) => {
+                dispatch({type: 'TOKEN_VALIDATED', payload: res.data.valid})
+            }).catch(e => dispatch({type: 'TOKEN_VALIDATED', payload: false}));
+        } else {
+            dispatch({type: 'TOKEN_VALIDATED', payload: false})
+        }
+    }
+}
---
+++ app/src/app/constants.js
@@ -0,0 +1,4 @@
+export default {
+    API_URL: 'http://localhost:3003/api',
+    AUTH_URL: 'http://localhost:3003/auth'
+}
